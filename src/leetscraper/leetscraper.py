cb9a13072437f4e01a05dbc379079f868db3397d
pavocracy <pavocracy@pm.me>
2022-02-11 10:13:01 +1000

    Macos try again

--- src/leetscraper/leetscraper.py
+++ src/leetscraper/leetscraper.py
@@ -97,7 +97,7 @@ class Leetscraper:
         if platform.startswith("darwin"):
             check_chrome_version = run(
                 [
-                    "/Applications/Google Chrome.app/Contents/MacOS/Google",
+                    "/Applications/Google Chrome.app",
                     "Chrome",
                     "--version",
                 ],
