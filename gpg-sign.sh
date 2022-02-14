f2deb7baf99eeca93e1e4a080db8f5dea3426054
pavocracy <pavocracy@pm.me>
2022-02-14 12:36:21 +1000

    Fix signing

--- gpg-sign.sh
+++ gpg-sign.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-for filename in dist/*
-do
-  gpg --detach-sign $filename
-  gpg --verify $filename.sig $filename
-done;
-
 leetscraper=src/leetscraper/leetscraper.py
 gpg --detach-sign $leetscraper
 gpg --verify $leetscraper.sig $leetscraper
