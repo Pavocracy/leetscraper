4ed390130d8f5772b7bd3d23a9dcaff7cfc84933
pavocracy <pavocracy@pm.me>
2022-02-19 08:28:31 +1000

    ascii art

--- src/leetscraper/leetscraper.py
+++ src/leetscraper/leetscraper.py
@@ -2,7 +2,15 @@
 # Signed using RSA key 9A5D2D5AA10873B9ABCD92F1D959AEE8875DEEE6
 # This file is part of leetscraper which is released under GPL-2.0 License.
 
-"""Leetscraper, a coding challenge webscraper for leetcode, and other websites!
+"""
+ _           _                                      
+| | ___  ___| |_ ___  ___ _ __ __ _ _ __   ___ _ __ 
+| |/ _ \/ _ \ __/ __|/ __| '__/ _` | '_ \ / _ \ '__|
+| |  __/  __/ |_\__ \ (__| | | (_| | |_) |  __/ |   
+|_|\___|\___|\__|___/\___|_|  \__,_| .__/ \___|_|   
+                                   |_|              
+
+a coding challenge webscraper for leetcode, and other websites!
 
 This module contains the Leetscraper class that when given the name of a supported
 website, will set some attributes that will allow coding challenges to be requested,
