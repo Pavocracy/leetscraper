acdca45bf0eb418228ed7d738e772bc64b6aba49
Adam Pavey <apavey@euclideon.com>
2022-04-28 16:52:35 +1000

    Add Firefox Support

--- tests/test_hackerrank.py
+++ tests/test_hackerrank.py
@@ -1,6 +1,7 @@
 import unittest
+import logging
 from shutil import rmtree
-from os import path, walk
+from os import path
 from src.leetscraper import Leetscraper  # type: ignore[import]
 
 
@@ -21,34 +22,43 @@ class TestLeetscraper(unittest.TestCase):
         )
         self.assertTrue(path.isdir(leetscraper.scraped_path))
 
-        # Check chrome_version
-        self.assertTrue(leetscraper.chrome_version)
+        # Check supported_browsers
+        avaliable_browsers = leetscraper.check_supported_browsers()
+        self.assertTrue(avaliable_browsers)
+
+        # Set browser and version for request header (hackerrank only)
+        leetscraper.browser = list(avaliable_browsers.keys())[0]
+        leetscraper.browser_version = avaliable_browsers[leetscraper.browser]
+
+        # Create a list of browsers to test
+        browsers = []
+        for browser, version in avaliable_browsers.items():
+            browsers.append({browser: version})
 
         # Check needed_problems
         scraped_problems = []
         needed_problems = leetscraper.needed_problems(scraped_problems)
         self.assertGreater(len(needed_problems), 1000)
 
-        # Check scrape_problems with scrape_limit
-        leetscraper.scrape_problems(needed_problems)
-        for (dirpath, dirnames, filenames) in walk(
-            f"{leetscraper.scraped_path}/PROBLEMS/{leetscraper.website_name}"
-        ):
-            for file in filenames:
-                if file:
-                    scraped_problems.append(file)
-        self.assertEqual(
-            len(scraped_problems), (leetscraper.scrape_limit - leetscraper.errors)
-        )
+        # Check create driver with all browsers
+        for index, browser in enumerate(browsers):
+            driver = leetscraper.create_webdriver(browser)
 
-        # Check scraped_problems
-        scraped_problems = leetscraper.scraped_problems()
-        self.assertEqual(
-            len(scraped_problems), (leetscraper.scrape_limit - leetscraper.errors)
-        )
+            # Check scrape_problems with scrape_limit
+            leetscraper.scrape_problems(
+                needed_problems[index * leetscraper.scrape_limit :], driver
+            )
 
-        # Cleanup problems
-        rmtree(leetscraper.scraped_path + "/PROBLEMS")
+            # Check scraped_problems
+            scraped_problems = leetscraper.scraped_problems()
+            self.assertEqual(
+                len(scraped_problems),
+                ((leetscraper.scrape_limit * (index + 1)) - leetscraper.errors),
+            )
+
+        # Cleanup problems and logging
+        rmtree(leetscraper.scraped_path)
+        logging.shutdown()
 
 
 if __name__ == "__main__":
