86ab88e240bae7bbeb050930f05b485652e317ac
Pavocracy <Pavocracy@users.noreply.github.com>
2022-05-06 14:57:01 +0000

    Release New Leetscraper Version

--- src/leetscraper/__init__.py
+++ src/leetscraper/__init__.py
@@ -1,10 +1,10 @@
 # Copyright (C) 2022 Pavocracy <pavocracy@pm.me>
-# Signed using RSA key 2.0.0D5AA10873B9ABCD92F1D959AEE8875DEEE6
+# Signed using RSA key 2.0.1D5AA10873B9ABCD92F1D959AEE8875DEEE6
 # This file is released as part of leetscraper under GPL-2.0 License.
 
 """leetscraper, a coding challenge webscraper for leetcode, and other websites!"""
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __licence__ = "GPL-2.0"
 __author__ = "Pavocracy"
 __email__ = "pavocracy@pm.me"
