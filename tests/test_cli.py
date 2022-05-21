9d09ae5674917019b43d824baba7d24ea799acc2
Pavocracy <pavocracy@pm.me>
2022-05-21 15:37:40 +1000

    test pipe

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 import unittest
 from os import path
-from subprocess import run
+from subprocess import run, PIPE
 
 from src.leetscraper.logger import log_message
 from src.leetscraper.cli import __version__
@@ -11,12 +11,13 @@ class TestLeetscraper(unittest.TestCase):
         cli_path = path.abspath("src/leetscraper/cli.py")
         log_message("warning", "trying to run %s", cli_path)
         output = run(["python3", cli_path, "-v"],
-                     capture_output=True, check=True, shell=True)
-        if str(output.stderr):
-            log_message("error", "stderr! %s", output.stderr)
-        if str(output.stdout):
-            log_message("warning", "Checking if leetscraper v%s in %s", __version__, str(output.stdout))
-        self.assertTrue(f"leetscraper v{__version__}" in str(output.stdout))
+                     capture_output=True, check=True, shell=True, stdout=PIPE,
+                     stderr=PIPE)
+        log_message("warning",
+                    "Checking if leetscraper v%s in %s",
+                    __version__,
+                    output)
+        self.assertTrue(f"leetscraper v{__version__}" in output)
 
 
 if __name__ == "__main__":
