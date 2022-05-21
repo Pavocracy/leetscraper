33ba16a4ac35bbe5e8d286401dc8d3e7d779bff0
Pavocracy <pavocracy@pm.me>
2022-05-21 14:55:40 +1000

    try again

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 import unittest
 from subprocess import run
 
+from src.leetscraper.logger import log_message
 from src.leetscraper.cli import __version__
 
 
@@ -8,7 +9,7 @@ class TestLeetscraper(unittest.TestCase):
     def test_cli(self):
         output = run(["python3", "src/leetscraper/cli.py", "-v"],
                      capture_output=True, check=True, shell=True)
-        print(f"Check if leetscraper v{__version__} in {output}")
+        log_message("warning", "Checking if leetscraper v%s in %s", __version__, str(output.stdout))
         self.assertTrue(f"leetscraper v{__version__}" in str(output.stdout))
 
 
