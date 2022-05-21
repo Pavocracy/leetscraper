64b2e2afd7c961812db8392c1091573b640e1da2
Pavocracy <pavocracy@pm.me>
2022-05-21 14:59:20 +1000

    check abspath

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import unittest
+from os import path
 from subprocess import run
 
 from src.leetscraper.logger import log_message
@@ -7,7 +8,9 @@ from src.leetscraper.cli import __version__
 
 class TestLeetscraper(unittest.TestCase):
     def test_cli(self):
-        output = run(["python3", "src/leetscraper/cli.py", "-v"],
+        cli_path = path.abspath("src/leetscraper/cli.py")
+        log_message("warning", "trying to run %s", cli_path)
+        output = run(["python3", cli_path, "-v"],
                      capture_output=True, check=True, shell=True)
         log_message("warning", "Checking if leetscraper v%s in %s", __version__, str(output.stdout))
         self.assertTrue(f"leetscraper v{__version__}" in str(output.stdout))
